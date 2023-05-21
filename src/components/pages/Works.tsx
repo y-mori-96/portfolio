a6a96bf69ae7da94acb293132180d24f047f185c
NonGlutinousRice <537.suteaka@gmail.com>
2023-05-21 16:08:12 +0900

    制作物　一旦完了

--- src/components/pages/Works.tsx
+++ src/components/pages/Works.tsx
@@ -1,3 +1,5 @@
+// スタイル
+import styled from 'styled-components';
 // コンポーネント
 import Title from '../../atoms/Title'
 import WorksList from '../organisms/WorksList'
@@ -6,9 +8,30 @@ const Works = () => {
   return (
     <>
       <Title title="制作物" />
+      <Test>
+        <Header3>個人開発</Header3>
+      </Test>
       <WorksList />
     </>
   )
 }
 
 export default Works
+
+/**
+ * スタイル
+ */
+/** カード
+ **************************************/
+const Test = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  text-align: center;
+  line-height: 50px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 2rem;
+`;
+const Header3 = styled.h3`
+  font-size: 2rem;
+`;
