6695275fd4018087e2a47ba893d74af59296d67e
NonGlutinousRice <y.mori2101@gmail.com>
2023-06-21 02:31:38 +0900

    【制作物】データ追加

--- src/types/workData.ts
+++ src/types/workData.ts
@@ -7,16 +7,28 @@ export type WorkData = {
   data: string;
   skills: string;
   categories: string;
+  modal: string;
 };
 
 export const worksData: WorkData[] = [
   {
     imageSrc: noImage,
-    title: "FOREST-CODE",
-    outline: "日々の学習を記録するためのサイト",
+    title: "森のポートフォリオ",
+    outline: "ポートフォリオサイト",
     data: "日",
     // skills: "TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS ",
     skills: "",
     categories: "",
+    modal: "portfolio",
+  },
+  {
+    imageSrc: noImage,
+    title: "FOREST-CODE",
+    outline: "学習記録サイト",
+    data: "日",
+    // skills: "TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS TypeScript React HTML CSS ",
+    skills: "",
+    categories: "",
+    modal: "forestCode",
   },
 ];
