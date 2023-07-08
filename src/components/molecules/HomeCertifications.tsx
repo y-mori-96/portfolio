342dd58ccd89304464a06d5274cc6e497d2e02cb
NonGlutinousRice <y.mori2101@gmail.com>
2023-07-08 21:24:37 +0900

    【ホーム】資格　完了

--- src/components/molecules/HomeCertifications.tsx
+++ src/components/molecules/HomeCertifications.tsx
@@ -9,42 +9,93 @@ import Certification from '../../images/certification.png';
 
 const HomeCertifications: React.FC = () => {
   return (
-    <article>
+    <Article>
       <Container>
         <Body>
           <HomeTitle title="資格" />
-          <Text>体系的な知識習得も欠かせません</Text>
+          <ResponsiveImgWrapper>
+            <ResponsiveImg src={Certification} alt="資格"/>
+          </ResponsiveImgWrapper>
+          <Text>体系的な知識習得も欠かしません</Text>
           <SeeMoreButton url="/certifications" />
         </Body>
         <ImgWrapper>
           <Img src={Certification} alt="資格"/>
         </ImgWrapper>
       </Container>
-    </article>
+    </Article>
   );
 };
 
 export default HomeCertifications;
 
+const Article = styled.article`
+  @media (max-width: 600px) {
+    margin-bottom: 5rem;
+  }
+`;
 const Container = styled.div`
   display: flex;
   align-items: center;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
 const ImgWrapper = styled.div`
   width: 50%;
   height: 400px;
+  @media (max-width: 768px) {
+    height: 300px;
+  }
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 const Img = styled.img`
   width: 100%;
   height: 100%;
   background-color: #fff;
+  object-fit: contain;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+
+const ResponsiveImgWrapper = styled.div`
+  display: none;
+  @media (max-width: 600px) {
+    display: block;
+    width: 100%;
+    height: 200px;
+    margin-bottom: 1rem;
+  }
+`;
+const ResponsiveImg = styled.img`
+  display: none;
+  @media (max-width: 600px) {
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    object-fit: contain;
+  }
 `;
 
 const Body = styled.div`
   width : 50%;
+  @media (max-width: 600px) {
+    width : 70%;
+  }
 `;
+
 const Text = styled.p`
   text-align: center;
   font-size: 2rem;
   margin-bottom: 2rem;
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 600px) {
+    font-size: 1rem;
+  }
 `;
