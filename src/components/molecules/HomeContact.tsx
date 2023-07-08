a90b652f71d1a808576d31d48969103a18090c43
NonGlutinousRice <y.mori2101@gmail.com>
2023-07-08 16:44:57 +0900

    【トップ】お問い合わせ一旦完了

--- src/components/molecules/HomeContact.tsx
+++ src/components/molecules/HomeContact.tsx
@@ -4,21 +4,49 @@ import styled from 'styled-components';
 // 型
 import HomeTitle from '../atoms/Titles/HomeTitle';
 import SeeMoreButton from '../atoms/buttons/SeeMoreButton';
+// 画像
+import Email from '../../images/email.png';
 
 const HomeContact: React.FC = () => {
   return (
-    <>
-      <HomeTitle title="お問い合わせ" />
-      <Text>詳細ページよりお問い合わせください</Text>
-      <SeeMoreButton url="/contact" />
-    </>
+    <article>
+      <Container>
+        <ImgWrapper>
+          <Img src={Email} alt="お問い合わせ"/>
+        </ImgWrapper>
+        <Body>
+          <HomeTitle title="お問い合わせ"/>
+          <Text>
+            詳細ページより<br/>
+            お問い合わせください
+          </Text>
+          <SeeMoreButton url="/contact" />
+        </Body>
+      </Container>
+    </article>
   );
 };
 
 export default HomeContact;
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ImgWrapper = styled.div`
+  width: 50%;
+  height: 400px;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const Body = styled.div`
+  width : 50%;
+`;
 const Text = styled.p`
   text-align: center;
-  font-size: 2.5rem;
+  font-size: 2rem;
   margin-bottom: 2rem;
 `;
