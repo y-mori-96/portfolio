54a64a5f31e90c082b79708c57f1c8d7d958b104
NonGlutinousRice <y.mori2101@gmail.com>
2023-07-08 20:18:55 +0900

    【ホーム】私について完了

--- src/components/molecules/HomeAbout.tsx
+++ src/components/molecules/HomeAbout.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 // 型
 import HomeTitle from '../atoms/Titles/HomeTitle';
 import SeeMoreButton from '../atoms/buttons/SeeMoreButton';
-
-import noImage from '../../images/noImg.png';
+// 画像
+import homeAbout from '../../images/home-about.jpg';
 
 const HomeAbout: React.FC = () => {
   return (
@@ -13,10 +13,18 @@ const HomeAbout: React.FC = () => {
       <HomeTitle title="私について" />
       <Wrapper>
         <ImgWrapper>
-          <Img src={noImage} alt="森湧汰" />
+          <Img src={homeAbout} alt="森湧汰" />
         </ImgWrapper>
         <Body>
-          <Name><ruby>森湧汰<rp>（</rp><rt>Mori Yuta</rt><rp>）</rp></ruby></Name>
+          <Name>
+          <ruby>
+            森<rp>（</rp><rt>Mori</rt><rp>）</rp>
+          </ruby>
+          <span> </span>
+          <ruby>
+            湧汰<rp>（</rp><rt>Yuta</rt><rp>）</rp>
+          </ruby>
+          </Name>
 
           <Text>
             1996年、兵庫県出身。<br />
@@ -26,12 +34,18 @@ const HomeAbout: React.FC = () => {
           </Text>
           <Text>
             現在、<br/>
-            プログラミングスクールで、PHP/Laravel/MySQLなどのバックエンドのスキルを学習した後、<br/>
-            職業訓練校で、illustrator/Photoshop/XDなどのデザインツール、<br/>
-            HTML/CSS/Sass/jQueryなどのフロントエンドのスキルを学習。<br/>
+            プログラミングスクールで、<br/>
+            PHP/Laravel/MySQLなどのバックエンドのスキルを学習した後、<br/>
           </Text>
           <Text>
-            「一人でECサイトを構築できる」スキルを持つWebエンジニアを目指す。
+            職業訓練校で、<br/>
+            illustrator/Photoshop/XDなどのデザインツール、<br/>
+            HTML/CSS/Sass/jQueryなどのフロントエンドのスキル、<br/>
+            WordPressを学習。<br/>
+          </Text>
+          <Text>
+            「一人でECサイトを構築できる」<br/>
+            スキルを持つWebエンジニアを目指す。
           </Text>
         </Body>
       </Wrapper>
@@ -43,35 +57,45 @@ const HomeAbout: React.FC = () => {
 export default HomeAbout;
 
 const Article = styled.article`
-  margin-bottom: 2rem;
+  margin-bottom: 10rem;
 `;
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: stretch;
+
+  @media (max-width: 768px){
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 const ImgWrapper = styled.div`
   width: 40%;
   padding: 0 2rem;
 
-  border: 1px solid;
+  @media (max-width: 768px){
+    width: 70%;
+    margin-bottom: 2rem;
+  }
 `;
 const Img = styled.img`
   width: 100%;
   `;
 const Body = styled.div`
   width: 60%;
-
-  border: 1px solid;
 `;
 
 const Name = styled.p`
-  font-size: 2.5rem;
+  font-size: 3.5rem;
   margin-bottom: 2rem;
 `;
 const Text = styled.p`
   text-align: left;
   font-size: 1.5rem;
-  line-height: 1.3;
+  line-height: 1.5;
   margin-bottom: 2rem;
+
+  @media (max-width: 600px){
+    font-size: 1rem;
+  }
 `;
